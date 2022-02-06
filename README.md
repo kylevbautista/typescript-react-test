1156a31fc2ded61ec46a8591ed3ecc3c51a8ad44
kyle.bautista <bautista.kvb@gmail.com>
2022-02-05 22:46:44 -0800

    install emotion package

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 - [x] install typescript
 - [x] initialize react project using create-react-app with typscript
+- [x] install emotion to used styled components and use css inside jsx
 - [] create simple react app using typscript
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,27 @@
+import React, { FC } from "react";
+
+import styled from "@emotion/styled";
+
+// adding a ? makes variable optional in an interface
 interface HomeProps {
   x?: string;
   y?: string;
 }
 
-import React, { FC } from "react";
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Home: FC<HomeProps> = (props) => {
   const x = props.x;
   const y = props.y;
 
-  return <div></div>;
+  return (
+    <Container>
+      <div>Hello</div>
+    </Container>
+  );
 };
 
 export default Home;
